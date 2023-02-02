9d18fe9a6e0cc28356f52d2837a6495ae29739f7
ephraim <mekgwele@gmail.com>
2023-02-02 16:38:40 +0200

    Update question-2.php

--- question-2/question-2.php
+++ question-2/question-2.php
@@ -4,20 +4,29 @@
 function checkString($inputString){
 
   $stringArray = explode(" ", $inputString);
+
+  // For multitple emails if need be.
   $emailsFound = [];
+
   for ($i=0; $i < count($stringArray) ; $i++) {
     if (strpos($stringArray[$i], "@") !== false && strpos($stringArray[$i], ".") !== false) {
-      // echo $stringArray[$i];
-      array_push($emailsFound ,$stringArray[$i]);
+      // if more than 1 email push to array
+      // array_push($emailsFound ,$stringArray[$i]);
+
+      // If only 1 email return here
+      return $stringArray[$i];
     }
   }
-
-  return $emailsFound;
+  // If more than one email return array here
+  // return $emailsFound;
 }
 
 $userString = "This is a string that contains an email address, and it has to be found, find the email find@me.com in it, also find mekgwele@gmail.com how about just mekgwele@gmail or mekgwele.com";
 
-echo implode(", ",checkString($userString))
+// If more than 1 email use below code
+// echo implode(", ",checkString($userString))
 
+// If only 1 email use below code
+echo checkString($userString)
 
  ?>
